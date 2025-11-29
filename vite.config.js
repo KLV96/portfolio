57bc20fd5dbd712194574d056ca54d33d950fc97
Rewaz KLV <rewaz96@hotmail.com>
2025-11-29 17:21:44 +0000

    Configure Vite for multi-page build

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,15 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
     base: '/portfolio/',
+    build: {
+        rollupOptions: {
+            input: {
+                main: 'index.html',
+                about: 'about.html',
+                contact: 'contact.html',
+                projects: 'projects.html',
+                journey: 'journey.html',
+            },
+        },
+    },
 })
